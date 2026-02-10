582603e076d847bbcae6f2bf7681b90e080de394
Shenbaga Jothi S <147464486+3012shenba@users.noreply.github.com>
2026-02-10 10:42:45 +0530

    Time: 8 ms (2.34%), Space: 52.8 MB (10.86%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String>list=new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            String s="";
+            if(i%3==0&&i%5==0)
+            {
+                list.add("FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                list.add("Fizz");
+            }
+            else if(i%5==0)
+            {
+                list.add("Buzz");
+            }
+            else
+            {
+                 s+=i;
+                list.add(s);
+            }
+        }
+        return list;
+        
+    }
+    
+}
