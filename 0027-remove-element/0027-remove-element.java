e0d553560da1cd060599b0f04e8c5db7c0d44314
Shenbaga Jothi S <147464486+3012shenba@users.noreply.github.com>
2026-02-10 13:52:46 +0530

    Time: 1 ms (3.57%), Space: 43.8 MB (20.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,11 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
      ArrayList<Integer>cl=new ArrayList();
+     int c=0;
      for(int i=0;i<nums.length;i++)
      {
         if(nums[i]!=val)
         {
            cl.add(nums[i]);
+           c++;
         }
      }
      for(int i=0;i<cl.size();i++)
@@ -13,7 +15,7 @@ class Solution {
        nums[i]= cl.get(i); 
        
      }
-     return cl.size();     
+     return c;     
 
     }
 }
